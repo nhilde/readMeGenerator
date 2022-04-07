af882798b0167c9619ef24ab384d33b81aaa6883
Noah Hildebrand <nhilde21@gmail.com>
2022-04-06 23:00:55 -0400

    worked on functions

--- assets/index.js
+++ assets/index.js
@@ -3,8 +3,9 @@
 // TODO: Create an array of questions for user input
 const inquirer = require('inquirer');
 
-inquirer
-  .prompt([
+
+const askUser = () => {
+  return inquirer.prompt([
     {
       type: 'input',
       message: 'What is the project title?',
@@ -17,57 +18,70 @@ inquirer
     },
     {
       type: 'input',
-      message: 'Do you want to include a table of contents?',
-      name: 'tableOfContents',
+      message: 'How do you install this application?',
+      name: 'installation',
     },
     {
-        type:'input',
-        message:'How do you install this application?',
-        name:'installation',
+      type: 'input',
+      message: 'How is the application used?',
+      name: 'usage',
     },
     {
-        type:'input',
-        message:'How is the application used?',
-        name:'usage',
+      type: 'input',
+      message: 'Enter the file path to the application demonstration',
+      name: 'demo',
     },
     {
-        type:'input',
-        message:'Do you want to credit anyone / anything?',
-        name:'credits',
+      type: 'input',
+      message: 'Enter any credits',
+      name: 'credits',
     },
     {
-        type:'input',
-        message:'Do you want to include a license?',
-        name:'license',
+      type: 'list',
+      message: 'Which License would you like to include?',
+      name: 'license',
+      choices: [
+        'MIT',
+        'GNU GPLv3',
+        'ISC',
+      ]
     },
     {
-        type:'input',
-        message:'Do you want to inlcude any badges?',
-        name:'badges',
+      type: 'input',
+      message: 'Do you want to inlcude any badges?',
+      name: 'badges',
     },
     {
-        type:'input',
-        message:'Do you want to include a list of features?',
-        name:'features',
+      type: 'input',
+      message: 'Do you want to include a list of features?',
+      name: 'features',
     },
     {
-        type:'input',
-        message:'Do you want to inlcude a section on how to contribute?',
-        name:'contribute',
+      type: 'input',
+      message: 'Enter details on how to contribute',
+      name: 'contribute',
     },
     {
-        type:'input',
-        message:'Do you want to include tests for your project?',
-        name:'tests',
+      type: 'input',
+      message: 'Do you want to include tests for your project?',
+      name: 'tests',
     },
-  ])
-  .then((response) =>
-    console.log(response),
-    console.log("test")
-  );
+    {
+      type: 'input',
+      message: 'Enter your GitHub username',
+      name: 'github',
+    },
+    {
+      type: 'input',
+      message: 'Enter your email address',
+      name: 'email',
+    },
+  ]);
+};
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {
+const generateMarkdown = ({ projectName, description, installation, usage, demo, credits, license, features, contribute, tests, email, github, badges }) => {
 
   `# ${projectName}
 
@@ -77,8 +91,6 @@ function writeToFile(fileName, data) {
   
   ## Table of Contents
   
-  ${tableOfContents}
-  
   - [Installation](#installation)
   - [Usage](#usage)
   - [Credits](#credits)
@@ -86,50 +98,55 @@ function writeToFile(fileName, data) {
   - [Badges](#badges)
   - [Features](#features)
   - [How to Contribute](#how to contribute)
+  - [Tests](#tests)
+  - [Questions](#questions)
   
   ## Installation
   
-  
+  ${installation}
   
   ## Usage
   
-  
-      ![alt text](assets/images/screenshot.png)
+  ${usage}
+
+      ![${projectName}](${demo})
       
   
   ## Credits
   
-  
+  ${credits}
   
   ## License
   
-  
+  Licensed under the ${license} license
   
   ## Badges
   
-  
+  ${badges}
   
   ## Features
   
-  
+  ${features}
   
   ## How to Contribute
   
-  
+  ${contribute}
   
   ## Tests
 
-
+  ${tests}
 
   ## Questions
 
-
+  For any further information, you can email me at ${email} or my GitHub username ${github}
 
 `
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  askUser()
+ }
 
 // Function call to initialize app
-init();
+init()
